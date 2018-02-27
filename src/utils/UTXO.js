58c1d786b4064b5a777cf5a19f793cf31baadcd0
nuanbing <vvvincentvan@gmail.com>
2018-02-27 11:52:23 +0800

    history page

---
+++ src/utils/UTXO.js
@@ -0,0 +1,43 @@
+import { WWW } from './API';
+import * as NEL from '../lib/neo-ts/index';
+export class UTXO {
+    static assets = {}
+    static history=[]
+    constructor() {
+
+    }
+    static async GetAssets(addr) {
+        var utxos = await WWW.api_getUTXO(addr);
+        console.log(utxos)
+        this.assets = {};
+        for (var i in utxos) {
+            var item = utxos[i];
+            var txid = item.txid;
+            var n = item.n;
+            var asset = item.asset;
+            var count = item.value;
+            if (this.assets[asset] == undefined) {
+                this.assets[asset] = [];
+            }
+            var utxo = new Utxo();
+            utxo.addr = item.addr;
+            utxo.asset = asset;
+            utxo.n = n;
+            utxo.txid = txid;
+            utxo.count = NEL.neo.Fixed8.parse(count);
+            let type = asset[2]==='6'?'GAS':'NEO'
+            this.history.push({asset:type,txid:txid,count:count})
+            this.assets[asset].push(utxo);
+        }
+        console.log(this.assets);
+        console.log(this.history)
+    }
+}
+
+export class Utxo {
+    addr = '';
+    txid = '';
+    n = -1;
+    asset = '';
+    count = 0;
+}
