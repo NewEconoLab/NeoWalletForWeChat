93909f71f9a86856c917230ac1cc06855ed0e074
nuanbing <jinghui@wayne.edu>
2018-04-23 19:03:30 +0800

    增加网络状态

---
+++ src/utils/const.ts
@@ -0,0 +1,32 @@
+/**
+ * 用户code 换取 session_key
+ * @type {String}
+ */
+export const USER_SPECICAL_INFO: string = "userSpecialInfo";
+
+/**
+ * 用户信息
+ * @type {String}
+ */
+export const USER_INFO: string = "userInfo";
+
+/**
+ * 缓存钱包列表
+ */
+export const LOCAL_ACCOUNTS: string = "localWallets";
+/** 
+ * 当前登陆账户
+*/
+export const CURR_ACCOUNT: string = "currentWallet";
+
+export const SCRYPT: any = {
+    N: 16384,
+    r: 8,
+    p: 8
+};
+
+export const NET_STATE: any = {
+    TEST: 'test_net',
+    MAIN: 'main_net'
+}
+
